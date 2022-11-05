4eaa2a010e05425850ce728ad37c54b4841e01a7
carlodavid225 <dcarlo318@gmail.com>
2022-11-05 09:55:36 +0800

    finifh

--- peys app/peysapp.php
+++ peys app/peysapp.php
@@ -25,7 +25,7 @@
     }
     ?>
   
-  <img src="deku.jfif" style="width: <?php echo $ImageRangeSize . '%'; ?>;border-color: <?php echo $border; ?>;border-width: 5px; border-style: solid;">
+  <img src="img/img.jpg.jpg" style="width: <?php echo $ImageRangeSize . '%'; ?>;border-color: <?php echo $border; ?>;border-width: 5px; border-style: solid;">
 
     </form>
 
